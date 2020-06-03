23d67afc6ee5cb7f1c75c46ba16c273e61aeadfa
Jon Rafkind <jon@rafkind.com>
2020-06-02 23:00:51 -0700

    start to decode instructions

--- cmd/nes/main.go
+++ cmd/nes/main.go
@@ -5,6 +5,7 @@ import (
     "log"
     "io"
     "bytes"
+    "math"
     "os"
 )
 
@@ -32,6 +33,71 @@ func isNes2(nesHeader []byte) bool {
     return nesHeader[7] & 0xc == 0x8
 }
 
+func readPRG(header []byte) uint64 {
+    lsb := header[4]
+    /* only use the lowest 4 bits of byte 9 in the header */
+    msb := header[9] & 15
+
+    if msb == 15 {
+        low2 := lsb & 3
+        exponent := lsb >> 2
+        return uint64(math.Pow(2.0, float64(exponent))) * uint64(low2*2 + 1)
+    } else {
+        return (uint64(msb << 8) + uint64(lsb)) << 14
+    }
+}
+
+func readCHR(header []byte) uint64 {
+    lsb := header[5]
+    msb := (header[9] >> 4) & 15
+    if msb == 15 {
+        low2 := lsb & 3
+        exponent := lsb >> 2
+        return uint64(math.Pow(2.0, float64(exponent))) * uint64(low2*2 + 1)
+    } else {
+        return (uint64(msb << 8) + uint64(lsb)) << 13
+    }
+}
+
+func decodeInstruction(high byte, low byte) string {
+    switch high {
+        case 0x0:
+            switch low {
+                case 0x0: return "BRK impl"
+                case 0x1: return "ORA x,ind"
+                case 0x2: return "STP"
+            }
+        case 0x2:
+            switch low {
+                case 0x0: return "JSR abs"
+            }
+        case 0x4:
+            switch low {
+                case 0xd: return "EOR abs"
+            }
+        case 0xa:
+            switch low {
+                case 0x9: return "LDA #"
+            }
+        case 0xe:
+            switch low {
+                case 0x6: return "INC zpg"
+                case 0xa: return "NOP"
+            }
+    }
+
+    return "?"
+}
+
+func dump_instructions(instructions []byte){
+    for instruction := 0; instruction < len(instructions); instruction++ {
+        lo := instructions[instruction] & 0xf
+        hi := instructions[instruction] >> 4
+
+        log.Printf("Instruction %v: Lo 0x%x Hi 0x%x = %v\n", instruction, lo, hi, decodeInstruction(hi, lo))
+    }
+}
+
 func parse(path string) error {
     file, err := os.Open(path)
     if err != nil {
@@ -54,6 +120,36 @@ func parse(path string) error {
     nes2 := isNes2(header)
 
     log.Printf("Nes 2.0 %v\n", nes2)
+
+    prgRomSize := readPRG(header)
+    chrRomSize := readCHR(header)
+
+    log.Printf("PRG-ROM %v\n", prgRomSize)
+    log.Printf("CHR-ROM %v\n", chrRomSize)
+
+    hasTrainer := (header[6] & 4) == 4
+    log.Printf("Has trainer area %v\n", hasTrainer)
+
+    log.Printf("Last 5 bytes: %v\n", header[11:])
+
+    if hasTrainer {
+        trainer := make([]byte, 512)
+        _, err = io.ReadFull(file, trainer)
+        if err != nil {
+            return err
+        }
+        log.Printf("Read trainer area\n")
+    }
+
+    prgRom := make([]byte, prgRomSize)
+
+    _, err = io.ReadFull(file, prgRom)
+    if err != nil {
+        return err
+    }
+
+    log.Printf("Read program data\n")
+    dump_instructions(prgRom)
 
     return nil
 }
