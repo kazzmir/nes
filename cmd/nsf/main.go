87a47b66e9fb292d611fb2be54a91f73986c57a3
Jon Rafkind <jon@rafkind.com>
2020-08-19 21:27:47 -0700

    reader all the header data

--- cmd/nsf/main.go
+++ cmd/nsf/main.go
@@ -42,6 +42,39 @@ func loadNSF(path string) (NSFFile, error) {
     totalSongs := header[0x6]
     startingSong := header[0x7]
 
+    loadAddress := (uint16(header[0x9]) << 8) | uint16(header[0x8])
+    initAddress := (uint16(header[0xb]) << 8) | uint16(header[0xa])
+    playAddress := (uint16(header[0xd]) << 8) | uint16(header[0xc])
+    songName := header[0xe:0xe+32]
+    artist := header[0x2e:0x2e+32]
+    copyright := header[0x4e:0x4e+32]
+    ntscSpeed := (uint16(header[0x6f]) << 8) | uint16(header[0x6f])
+    bankValues := header[0x70:0x78]
+    palSpeed := (uint16(header[0x79]) << 8) | uint16(header[0x78])
+    palOrNtsc := header[0x7a]
+
+    extraSoundChip := header[0x7b]
+    nsf2Reserved := header[0x7c]
+    nsf2MetaData := header[0x7d:0x7d+3]
+
+    _ = nsf2Reserved
+    _ = nsf2MetaData
+
+    log.Printf("Version %v", version)
+    log.Printf("Total songs %v", totalSongs)
+    log.Printf("Starting song %v", startingSong)
+    log.Printf("Load address 0x%x", loadAddress)
+    log.Printf("Init address 0x%x", initAddress)
+    log.Printf("Play address 0x%x", playAddress)
+    log.Printf("Song '%v'", string(songName))
+    log.Printf("Artist '%v'", string(artist))
+    log.Printf("Copyright '%v'", string(copyright))
+    log.Printf("NTSC speed %v", ntscSpeed)
+    log.Printf("Bank values %v", bankValues)
+    log.Printf("PAL speed %v", palSpeed)
+    log.Printf("PAL/NTSC %v", palOrNtsc)
+    log.Printf("Extra sound chip %v", extraSoundChip)
+
     _ = version
     _ = totalSongs
     _ = startingSong
