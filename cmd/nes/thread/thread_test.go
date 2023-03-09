7e9636d32e41f315e39b77d06298e1386f5b4323
Jon Rafkind <jon@rafkind.com>
2023-03-08 21:49:15 -0800

    add cancel test

--- cmd/nes/thread/thread_test.go
+++ cmd/nes/thread/thread_test.go
@@ -3,6 +3,7 @@ package main
 import (
     "testing"
     "context"
+    "time"
 )
 
 func TestBasic(test *testing.T){
@@ -23,4 +24,41 @@ func TestBasic(test *testing.T){
     if x != 10 {
         test.Fatalf("expected x to be 10 but was %v", x)
     }
+}
+
+func TestCancel(test *testing.T){
+    quit, cancel := context.WithCancel(context.Background())
+    defer cancel()
+    group := NewThreadGroup(quit)
+
+    x := 0
+
+    c := make(chan int)
+
+    group.Spawn(func (){
+        select {
+            case <-group.Done():
+                return
+            case <-c:
+                x = 2
+        }
+    })
+
+    group.Spawn(func (){
+        group.Cancel()
+        x = 1
+    })
+
+    go func(){
+        time.Sleep(5 * time.Millisecond)
+        c <- 3
+        close(c)
+    }()
+
+    group.Wait()
+
+    if x != 1 {
+        test.Fatalf("Expected x to be 1 but was %v", x)
+    }
+
 }
