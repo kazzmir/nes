0dd1d49be92995002bc9c75a4957ad93c28c37a4
Jon Rafkind <jon@rafkind.com>
2023-03-09 20:59:21 -0800

    add simple thread group utility (#30)
    * add thread groups
    
    * add cancel test
    
    * fix subgroup waiting
    
    * use thread groups in the rom loader

---
+++ cmd/nes/thread/thread.go
@@ -0,0 +1,86 @@
+package thread
+
+import (
+    "sync"
+    "context"
+)
+
+type ThreadGroup struct {
+    wait sync.WaitGroup
+    quit context.Context
+    cancel context.CancelFunc
+}
+
+type ThreadFuncCancel func(quit context.Context, cancel context.CancelFunc)
+type ThreadFunc func()
+
+func NewThreadGroup(parent context.Context) *ThreadGroup {
+    quit, cancel := context.WithCancel(parent)
+    out := &ThreadGroup{
+        quit: quit,
+        cancel: cancel,
+    }
+
+    return out
+}
+
+/* create a new group that can have its own set of threads.
+ * the current group will wait for all subgroups to exit
+ */
+func (group *ThreadGroup) SubGroup() *ThreadGroup {
+    out := NewThreadGroup(group.quit)
+
+    group.wait.Add(1)
+    go func(){
+        <-out.quit.Done()
+        out.wait.Wait()
+        defer group.wait.Done()
+    }()
+    
+    return out
+}
+
+func (group *ThreadGroup) SpawnWithCancel(f ThreadFuncCancel){
+    group.wait.Add(1)
+    go func(){
+        defer group.wait.Done()
+        f(group.quit, group.cancel)
+    }()
+}
+
+func (group *ThreadGroup) Spawn(f ThreadFunc) {
+    group.wait.Add(1)
+    go func(){
+        defer group.wait.Done()
+        f()
+    }()
+}
+
+func (group *ThreadGroup) SpawnN(f ThreadFunc, i int) {
+    for n := 0; n < i; n++ {
+        group.Spawn(f)
+    }
+}
+
+func (group *ThreadGroup) Cancel(){
+    group.cancel()
+}
+
+func (group *ThreadGroup) Context() context.Context {
+    return group.quit
+}
+
+func (group *ThreadGroup) Done() <-chan struct{} {
+    return group.quit.Done()
+}
+
+func (group *ThreadGroup) Wait(){
+    group.wait.Wait()
+    // calling cancel here ensures the parent group will be notified about this group
+    group.cancel()
+}
+
+func (group *ThreadGroup) CancelAndWait() {
+    group.cancel()
+    group.wait.Wait()
+}
---
+++ cmd/nes/thread/thread_test.go
@@ -0,0 +1,95 @@
+package thread
+
+import (
+    "testing"
+    "context"
+    "time"
+    _ "fmt"
+)
+
+func TestBasic(test *testing.T){
+    quit, cancel := context.WithCancel(context.Background())
+    defer cancel()
+    group := NewThreadGroup(quit)
+
+    var x int
+    
+    group.Spawn(func (){
+        for i := 0; i < 10; i++ {
+            x += 1
+        }
+    })
+
+    group.Wait()
+    
+    if x != 10 {
+        test.Fatalf("expected x to be 10 but was %v", x)
+    }
+}
+
+func TestCancel(test *testing.T){
+    quit, cancel := context.WithCancel(context.Background())
+    defer cancel()
+    group := NewThreadGroup(quit)
+
+    x := 0
+
+    c := make(chan int)
+
+    group.Spawn(func (){
+        select {
+            case <-group.Done():
+                return
+            case <-c:
+                x = 2
+        }
+    })
+
+    group.Spawn(func (){
+        group.Cancel()
+        x = 1
+    })
+
+    go func(){
+        time.Sleep(5 * time.Millisecond)
+        c <- 3
+        close(c)
+    }()
+
+    group.Wait()
+
+    if x != 1 {
+        test.Fatalf("Expected x to be 1 but was %v", x)
+    }
+
+}
+
+func TestSubGroup(test *testing.T){
+    quit, cancel := context.WithCancel(context.Background())
+    defer cancel()
+
+    group := NewThreadGroup(quit)
+
+    x := 0
+
+    group1 := group.SubGroup()
+    group1.Spawn(func(){
+        defer func(){
+            x = 3
+        }()
+        for {
+            select {
+                case <-group1.Done():
+                    return
+            }
+        }
+    })
+
+    time.Sleep(1 * time.Millisecond)
+    group.Cancel()
+    group.Wait()
+
+    if x != 3 {
+        test.Fatalf("Expected x to be 3 but was %v", x)
+    }
+}
