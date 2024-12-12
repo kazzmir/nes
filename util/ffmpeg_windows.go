d1f52bdfeea3e2f8a5e3a0dbb252c3be20a77abd
Jon Rafkind <jon@rafkind.com>
2024-12-12 22:00:03 +0000

    stub for windows

--- util/ffmpeg_windows.go
+++ util/ffmpeg_windows.go
@@ -1,2 +1,14 @@
 package util
 
+import (
+    "errors"
+    "context"
+
+    nes "github.com/kazzmir/nes/lib"
+)
+
+var UnsupportedError = errors.New("Unsupported")
+
+func RecordMp4(mainQuit context.Context, mp4Path string, overscanPixels int, sampleRate int, video_channel chan nes.VirtualScreen, audio_channel chan []float32) error {
+    return UnsupportedError
+}
