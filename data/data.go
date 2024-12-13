57ab7059322591a084550650b62728277a4d26fc
Jon Rafkind <jon@rafkind.com>
2024-12-13 08:44:31 +0000

    always use / as a separator

--- data/data.go
+++ data/data.go
@@ -3,12 +3,11 @@ package data
 import (
     "embed"
     "io/fs"
-    "path/filepath"
 )
 
 //go:embed data/*
 var dataFS embed.FS
 
 func OpenFile(path string) (fs.File, error) {
-    return dataFS.Open(filepath.Join("data", path))
+    return dataFS.Open("data/" + path)
 }
