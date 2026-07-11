729b587f05c7a469fbdbce966056b9807659df04
Jon Rafkind <jon@rafkind.com>
2026-07-11 16:36:47 -0700

    compute crc

--- lib/patch/bps.go
+++ lib/patch/bps.go
@@ -8,6 +8,10 @@ import (
     "errors"
 )
 
+var ErrSourceCRC = errors.New("source CRC mismatch")
+var ErrTargetCRC = errors.New("target CRC mismatch")
+var ErrPatchCRC = errors.New("patch CRC mismatch")
+
 func littleEndian(b []byte) uint32 {
     return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
 }
@@ -42,6 +46,23 @@ func readBPSVarInt(r *bytes.Reader) (uint64, error) {
     }
 
     return value, nil
+}
+
+func computeCRC32(data []byte) uint32 {
+    crc := uint32(0xFFFFFFFF)
+
+    for _, b := range data {
+        crc ^= uint32(b)
+        for range 8 {
+            if crc&1 > 0 {
+                crc = (crc >> 1) ^ 0xEDB88320
+            } else {
+                crc >>= 1
+            }
+        }
+    }
+
+    return ^crc
 }
 
 func ApplyBPSPatch(nesData []byte, patchData []byte) ([]byte, error) {
@@ -207,27 +228,37 @@ func ApplyBPSPatch(nesData []byte, patchData []byte) ([]byte, error) {
         }
     }
 
-    // FIXME: check crc
-    /*
-    sourceCRC, err := readUint32(reader)
+    crcBytes := make([]byte, 12)
+    copy(crcBytes, patchData[len(patchData)-12:])
+
+    crcReader := bytes.NewReader(crcBytes)
+
+    sourceCRC, err := readUint32(crcReader)
     if err != nil {
         return nil, err
     }
 
-    targetCRC, err := readUint32(reader)
+    targetCRC, err := readUint32(crcReader)
     if err != nil {
         return nil, err
     }
 
-    patchCRC, err := readUint32(reader)
+    patchCRC, err := readUint32(crcReader)
     if err != nil {
         return nil, err
     }
 
-    _ = sourceCRC
-    _ = targetCRC
-    _ = patchCRC
-    */
+    if computeCRC32(nesData) != sourceCRC {
+        return nil, ErrSourceCRC
+    }
+
+    if computeCRC32(target) != targetCRC {
+        return nil, ErrTargetCRC
+    }
+
+    if computeCRC32(patchData[:len(patchData)-4]) != patchCRC {
+        return nil, ErrPatchCRC
+    }
 
     return target, nil
 }
