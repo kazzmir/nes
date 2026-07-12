f1b810e13870f916817e7042d57e32ed5182fae0
Jon Rafkind <jon@rafkind.com>
2026-07-12 12:02:05 -0700

    patcher can apply multiple patches

--- cmd/patch/main.go
+++ cmd/patch/main.go
@@ -9,12 +9,11 @@ import (
 
 func main() {
     if len(os.Args) < 3 {
-        fmt.Printf("Usage: %s <rom_file> <ips_patch>\n", os.Args[0])
+        fmt.Printf("Usage: %s <rom_file> <patch1> [<patch2> ...]\n", os.Args[0])
         return
     }
 
     romFile := os.Args[1]
-    patchFile := os.Args[2]
 
     romBytes, err := os.ReadFile(romFile)
     if err != nil {
@@ -22,26 +21,30 @@ func main() {
         return
     }
 
-    patchBytes, err := os.ReadFile(patchFile)
-    if err != nil {
-        fmt.Printf("Error reading IPS patch file: %v\n", err)
-        return
+    for _, patchFile := range os.Args[2:] {
+        patchBytes, err := os.ReadFile(patchFile)
+        if err != nil {
+            fmt.Printf("Error reading patch file: %v\n", err)
+            return
+        }
+
+        fmt.Printf("Applying patch: %s to ROM: %s\n", patchFile, romFile)
+        fmt.Printf("Original ROM size: %d bytes\n", len(romBytes))
+        fmt.Printf("Patch size: %d bytes\n", len(patchBytes))
+
+        patchedRom, err := patch.ApplyPatch(romBytes, patchBytes)
+        if err != nil {
+            fmt.Printf("Error applying patch: %v\n", err)
+            return
+        }
+
+        fmt.Printf("Patched ROM size: %d bytes\n", len(patchedRom))
+
+        romBytes = patchedRom
     }
-
-    fmt.Printf("Applying IPS patch: %s to ROM: %s\n", patchFile, romFile)
-    fmt.Printf("Original ROM size: %d bytes\n", len(romBytes))
-    fmt.Printf("Patch size: %d bytes\n", len(patchBytes))
-
-    patchedRom, err := patch.ApplyIPSPatch(romBytes, patchBytes)
-    if err != nil {
-        fmt.Printf("Error applying IPS patch: %v\n", err)
-        return
-    }
-
-    fmt.Printf("Patched ROM size: %d bytes\n", len(patchedRom))
 
     outputFile := romFile + ".patched"
-    err = os.WriteFile(outputFile, patchedRom, 0644)
+    err = os.WriteFile(outputFile, romBytes, 0644)
     if err != nil {
         fmt.Printf("Error writing patched ROM file: %v\n", err)
         return
